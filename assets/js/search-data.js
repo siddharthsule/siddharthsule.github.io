a452ded3191a1102c616036c15c810eaca902e05
siddharthsule <63209784+siddharthsule@users.noreply.github.com>
2025-10-22 13:57:57 +0000

    Deploying to gh-pages from @ siddharthsule/siddharthsule.github.io@34889c290be5d5939a68edf864fdcc04ac3aea61 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,7 +35,7 @@ ninja.data = [{
           description: "My CV, available as both PDF and as this webpage. Simply click the pdf icon to download it.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/assets/pdf/Siddharth_Sule_CV.pdf";
           },
         },{id: "projects-parton-showers-on-gpus-with-gaps",
           title: 'Parton Showers on GPUs with GAPS',
